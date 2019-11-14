4f8616bcec38f6a152ec36af892ec6ca5ad931ff
alecseperack17 <55203347+alecseperack17@users.noreply.github.com>
2019-11-14 17:19:57 -0600

    Create MS SQL Inserts.sql

---
+++ MS SQL Inserts.sql
@@ -0,0 +1,59 @@
+INSERT INTO `locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0001', 'IL', 'MI', 'Atl');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0002', 'MI', 'IL', 'Cal');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0003', 'Atl', 'Cal', 'MI');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0004', 'NY', 'FL', 'Cn');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0005', 'FL', 'Pen', 'IN');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0006', 'Al', 'AK', 'CA');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0007', 'DE', 'CT', 'NE');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0008', 'OH', 'OR', 'SD');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0009', 'IA', 'MO', 'RI');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0010', 'OK', 'TN','TN');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0011', 'WY', 'WI', 'VT');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0012', 'ID', 'UT', 'WV');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0013', 'MO', 'ME', 'NC');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0014', 'MI', 'KS', 'ME');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0015', 'KY', 'KS', 'NM');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0016', 'SC', 'NC', 'OH');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0017', 'OH', 'ID', 'OK');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0018', 'TX', 'WI', 'UT');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0019', 'AK', 'CO', 'IA');
+
+INSERT INTO `Locations` ([LocationID], [production], [Warehousing], [Offices])
+VALUES ('0020', 'MT', 'RI', 'MA');
